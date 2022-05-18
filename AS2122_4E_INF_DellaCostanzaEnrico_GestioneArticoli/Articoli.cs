9daf911e2c2230e183a77f572415810e486b42d2
dellacostanzaenrico@gmail.com <PC07@INFO5-5378>
2022-05-18 09:53:04 +0200

    creazione classe articoli

---
+++ AS2122_4E_INF_DellaCostanzaEnrico_GestioneArticoli/Articoli.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2122_4E_INF_DellaCostanzaEnrico_GestioneArticoli
+{
+    internal class Articoli
+    {
+        string codice;
+        string unitadimisura;
+        string descrizione;
+        double prezzo;
+
+        string Codice { get { return codice; } };
+        string Unitadimisura { get { return unitadimisura; } }
+        string Descrizione { get { return descrizione; } }
+        double Prezzo { get { return prezzo; } }
+
+        public Articoli(string codice,string unitadimisura,string descrizione,double prezzo)
+        {
+            this.codice = codice;
+            this.unitadimisura = unitadimisura;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+        }
+
+
+
+
+    }
+
+}
