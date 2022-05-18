d7aea223770010bda4b6a3ede4f957997a9d5096
PC07 <PC07@INFO5-5378>
2022-05-18 09:13:55 +0200

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_DellaCostanzaEnrico_GestioneArticoli/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2122_4E_INF_DellaCostanzaEnrico_GestioneArticoli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
